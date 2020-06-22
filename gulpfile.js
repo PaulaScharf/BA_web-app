faedecc43f427794452d546852dd68f38516b78d
psch <p.scharf@conterra.de>
2020-06-22 15:54:43 +0200

    updated versions of dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,11 @@ const mapapps = require('ct-mapapps-gulp-js');
 mapapps.registerTasks({
     /* A detailed description of available setting is available at https://www.npmjs.com/package/ct-mapapps-gulp-js */
     /* a list of themes inside this project */
-    themes: ['theme-custom'],
+    themes: [],
     /* state that the custom theme will be dependant from map.apps everlasting theme that provides the base styles */
-    hasBaseThemes: true,
+    hasBaseThemes: false,
     /* state that we want to support vuetify components and therefore need the the vuetify core styles*/
-    hasVuetify: true,
-    themeChangeTargets: {
-        "vuetify": [
-            "theme-custom"
-        ]
-    }
+    hasVuetify: false
 });
 
 gulp.task("default",
